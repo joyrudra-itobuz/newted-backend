4ca3bb9fda7a4fab8a8ffc8f5220c0008f7e88ff
Joyrudra Biswas <joyrudra@itobuz.com>
2023-07-21 11:38:34 +0530

    main FIX : Added the Edit Note API

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,6 +1,12 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 import { response } from "../module/responseObject";
 import { noteModel, NoteModel } from "../models/note";
+import {
+  ReasonPhrases,
+  StatusCodes,
+  getReasonPhrase,
+  getStatusCode,
+} from "http-status-codes";
 
 export const addNewNote: RequestHandler = async (req, res, next) => {
   if (!req.body.heading) {
@@ -9,34 +15,71 @@ export const addNewNote: RequestHandler = async (req, res, next) => {
       .send(response(null, "Failed To Add a New Note 🙁!", false));
   }
 
-  const result = new noteModel(req.body);
+  try {
+    const result = new noteModel(req.body);
 
-  if (!result) {
-    return res
-      .status(404)
-      .send(response(null, "Failed To Add a New Note 🙁!", false));
-  }
+    if (!result) {
+      return res
+        .status(404)
+        .send(response(null, "Failed To Add a New Note 🙁!", false));
+    }
 
-  const newNote = await result.save();
+    const newNote = await result.save();
 
-  if (newNote) {
-    const note = await noteModel.findOne({ _id: newNote.id });
-    return res
-      .status(200)
-      .send(response(note, "Note Added Successfully ✅!", true));
+    if (newNote) {
+      const note = await noteModel.findOne({ _id: newNote.id });
+      return res
+        .status(200)
+        .send(response(note, "Note Added Successfully ✅!", true));
+    }
+  } catch (error: any) {
+    return next(error);
   }
 };
 
 export const getAllNotes: RequestHandler = async (req, res, next) => {
-  const result = await noteModel.find();
+  try {
+    const result = await noteModel.find();
 
-  if (!result) {
+    if (!result) {
+      return res
+        .status(404)
+        .send(response(null, "Failed To Fetch The Notes 🙁!", false));
+    }
+
     return res
-      .status(404)
-      .send(response(null, "Failed To Fetch The Notes 🙁!", false));
+      .status(200)
+      .send(response(result, "Here are all the Notes 📖!", true));
+  } catch (error: any) {
+    return next(error);
   }
-
-  return res
-    .status(200)
-    .send(response(result, "Here are all the Notes 📖!", true));
 };
+
+export const editNote: RequestHandler = async (req, res, next) => {
+  const noteID = req.body._id;
+  const updatedData = req.body;
+
+  if (!noteID) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .send(response(null, "Note Not Found 🙁!", false));
+  }
+  try {
+    const result = await noteModel.findByIdAndUpdate(noteID, updatedData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (result) {
+      return res
+        .status(StatusCodes.OK)
+        .send(response(result, "Note has been Updated ✅!", true));
+    } else {
+      throw new Error("Couldn't Update The Note 🙁!");
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,14 @@
-import { addNewNote, getAllNotes } from "../controller/userController";
+import {
+  addNewNote,
+  getAllNotes,
+  editNote,
+} from "../controller/userController";
 import express from "express";
 
 const app = express();
 
 app.get("/all-notes", getAllNotes);
 app.post("/new-note", addNewNote);
+app.patch("/edit-note", editNote);
 
 export default app;
